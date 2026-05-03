746fa32385514bab09c8a47911b19e6aa62aebe5
Zyrouge <hello@zyrouge.me>
2026-05-03 14:47:06 +0530

    fix: image version checking

--- release-info.sh
+++ release-info.sh
@@ -21,17 +21,17 @@ echo "$FIREFLYIII_VERSION" > "$DIST_DIR/version.txt"
 
 if [ "${FORCE:-}" != "true" ]; then
     set +e
-    IMAGE_MANIFEST=$(podman manifest inspect "$FIREFLYIII_IMAGE_NAME:$FIREFLYIII_VERSION" 2>&1)
+    IMAGE_MANIFEST=$(podman manifest inspect "$IMAGE_NAME:$FIREFLYIII_VERSION" 2>&1)
     FOUND_VERSION_EXIT_CODE="$?"
     set -e
+    if [ "$FOUND_VERSION_EXIT_CODE" -eq 0 ]; then
+        echo "Version $FIREFLYIII_VERSION already exists, skipping release..."
+        exit 0
+    fi
     if [ "$FOUND_VERSION_EXIT_CODE" -ne 125 ]; then
         echo "$IMAGE_MANIFEST"
         echo "Error: Failed to fetch image manifest"
         exit 1
-    fi
-    if [ "$FOUND_VERSION_EXIT_CODE" -eq 0 ]; then
-        echo "Version $FIREFLYIII_VERSION already exists, skipping release..."
-        exit 0
     fi
 fi
 
